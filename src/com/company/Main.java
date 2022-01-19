d32c3d8fb8c49274b42ded33f60deac6d8b3d595
Naida <naidaaa.bajic@gmail.com>
2022-01-19 12:44:05 +0100

    Java introduction zadaca

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Ja sam Naida Bajić Hasić.");
+        System.out.println("Rodjena sam i odrasla u Tuzli.");
+        System.out.println("Udata sam za svog kolegu iz skole programiranja i ponekad zaboravim da imam dva prezimena.");
+        System.out.println("Smatram da je Istanbul najljepsi grad na cijelom svijetu.");
+        System.out.println("Do sad sam posjetila 3 kontinenta. Europu, Aziju i Sjevernu Ameriku.");
+    }
+}
