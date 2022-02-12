e92c30bd5f7bef7f713ce7c07b096033799da4f8
Naida <naidaaa.bajic@gmail.com>
2022-02-12 15:24:59 +0100

    Homework_(7)9

---
+++ src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int A = 5;
+        int B = 6;
+
+        System.out.println("A" + " is: " +A + ", B " + "is: " +B);
+
+        A +=B;
+        System.out.println("Value of A after addition B is: " +A + ".");
+
+        A -=B;
+        System.out.println("Value of A after subtraction A is: " +A + ".");
+
+        A *=B;
+        System.out.println("Value of A after multiplication with B is: " +A + ".");
+
+        A /=B;
+        System.out.println("Value of A after division with B is: " +A + ".");
+
+        A %=B;
+        System.out.println("Value of A after using modulo is: " +A + ".");
+
+
+
+
+        String Lj1 = "Naida";
+        String Lj2 = "Izudin";
+
+
+        System.out.println(Lj1 + " + " + Lj2 + " = " + (Lj1 + Lj2) + " = Brak");
+
+
+    }
+}
